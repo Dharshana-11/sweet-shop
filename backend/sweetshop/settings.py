c295c79d3e7016a9ae7e487477d00e0a0407b174
Dharshana-11 <dharshana1103@gmail.com>
2025-12-13 17:56:22 +0530

    chore: created api app and wired it to DRF

--- backend/sweetshop/settings.py
+++ backend/sweetshop/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'api', # Local app,
+    'rest_framework' # Third-party API
 ]
 
 MIDDLEWARE = [
