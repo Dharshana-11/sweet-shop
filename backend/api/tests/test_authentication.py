421fcd580191b486af7c666fd7024f9403d4437e
Dharshana-11 <dharshana1103@gmail.com>
2025-12-13 20:07:08 +0530

    test: added failing tests for user login

--- backend/api/tests/test_authentication.py
+++ backend/api/tests/test_authentication.py
@@ -1,5 +1,6 @@
 from rest_framework.test import APITestCase
 from rest_framework import status
+from django.contrib.auth.models import User
 from django.urls import reverse
 
 class RegisterTestUser(APITestCase):
@@ -18,3 +19,30 @@ class RegisterTestUser(APITestCase):
 
         # Checks whether server responded with 201, if yes it passes silently; if no then will raise an error
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+class LoginTestUser(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username = "testuser",
+            password = "test@123"
+        )
+
+        self.url = reverse("login")
+    
+    def test_login_success(self):
+        data = {
+            "username" : "testuser",
+            "password" : "test@123" 
+        }
+
+        response = self.client.post(self.url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_login_fail(self):
+        data = {
+            "username" : "testuser",
+            "password" : "wrong@password"
+        }
+
+        response = self.client.post(self.url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
