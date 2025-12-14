3e140bfe59191eaefd51f32e37ee500cb995785e
Dharshana-11 <dharshana1103@gmail.com>
2025-12-14 14:49:44 +0530

    refactor: improve sweet model and clean sweets list view

--- backend/sweets/views.py
+++ backend/sweets/views.py
@@ -1,19 +1,28 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from .models import Sweets
-from .serailizers import SweetsSerializer
 from rest_framework import status
 
-@api_view(['GET','POST'])
+from .models import Sweet
+from .serializers import SweetSerializer
+
+
+@api_view(['GET', 'POST'])
+
+# Only logged-in users can access this API
 @permission_classes([IsAuthenticated])
 def sweets_list(request):
-    if request.method == "GET":
-        sweets = Sweets.objects.all()
-        serializer = SweetsSerializer(sweets, many=True)
-        return Response(serializer.data)
-    
-    serializer = SweetsSerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
+
+    # If request is GET, return all sweets from the DB
+    if request.method == 'GET':
+        queryset = Sweet.objects.all()  # fetch all sweet records
+        serializer = SweetSerializer(queryset, many=True)  # convert data to JSON
+        return Response(serializer.data)  # send data as response
+
+    # If the request is POST, create new sweet
+    serializer = SweetSerializer(data=request.data)  # take input data from request
+    serializer.is_valid(raise_exception=True)  
+    serializer.save()  
+
+    # Return created sweet data with success status
     return Response(serializer.data, status=status.HTTP_201_CREATED)
